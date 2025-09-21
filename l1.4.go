75cecc793fb98ce7c04f210a53881e6eb81b72a9
Edororo <eduardsimonyan1@mail.ru>
2025-09-21 17:58:54 +0300

    4 задание

--- l1.4.go
+++ l1.4.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	go func() {
+		defer wg.Done()
+		counter := 1
+
+		for {
+			select {
+			case <-ctx.Done():
+				close(ch)
+				return
+			default:
+				ch <- counter
+				fmt.Printf("Отправленное значение: %d\n", counter)
+				counter++
+				time.Sleep(20 * time.Millisecond)
+			}
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for value := range ch {
+			fmt.Printf("Полученное значение: %d\n", value)
+			time.Sleep(20 * time.Millisecond)
+		}
+	}()
+	wg.Wait()
+	fmt.Println("Программа завершена по Ctrl + C")
+}
