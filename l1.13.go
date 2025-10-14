f89885bdd11d781de411d5b70a04bb28a95abb7d
Edororo <eduardsimonyan1@mail.ru>
2025-10-14 22:19:31 +0300

    13 задание

--- l1.13.go
+++ l1.13.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b int
+	fmt.Scanln(&a, &b)
+	x1, y1 := XOR(a, b)
+	fmt.Println("XOR:", x1, y1)
+
+	fmt.Scanln(&a, &b)
+	x2, y2 := PlusMinus(a, b)
+	fmt.Println("Сложение и вычитание:", x2, y2)
+}
+
+func XOR(a, b int) (int, int) {
+	a = a ^ b
+	b = a ^ b
+	a = a ^ b
+	return a, b
+}
+
+func PlusMinus(a, b int) (int, int) {
+	a = a + b
+	b = a - b
+	a = a - b
+	return a, b
+}
